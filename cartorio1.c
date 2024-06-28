f63a4ca100ce2ba06c7a50f0612bb966a61e3929
Luiz Gomez <luizinchavao@gmail.com>
2024-06-27 23:39:39 -0300

    Autoria Luiz Gomez

--- cartorio1.c
+++ cartorio1.c
@@ -6,14 +6,13 @@
 int main () 
 {
       setlocale(LC_ALL, "portuguese");     
-      printf("Olá Mundo Olá Curso\n");
       printf("### Cartório da EBAC ###\n\n");
       printf("Escolha a Opção desejada no menu: \n\n");
       printf("\t1 - Registrar nomes:\n");
       printf("\t2 - Consultar nomes:\n");
       printf("\t3 - Deletar nomes:\n\n\n");  
       
-      printf("Esse Software é de livre uso dos alunos\n\n");	  
+      printf("Esse Software é de autoria Luiz Gomez\n\n");	  
 } 
    
    
