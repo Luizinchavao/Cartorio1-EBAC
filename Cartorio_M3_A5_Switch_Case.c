4166c7031b481ebcc5e271c24794adb80684318b
Luiz Gomez <luizinchavao@gmail.com>
2024-12-03 14:23:11 -0300

    Módulo 4 - Gerenciamento de Funções - Aula 2 - Banco de Dados

--- Cartorio_M3_A5_Switch_Case.c
+++ Cartorio_M3_A5_Switch_Case.c
@@ -1,26 +1,6 @@
 #include <stdio.h>      //biblioteca de texto de comunicação com usuário
 #include <stdlib.h>     //biblioteca de alocação de espaço em memória
 #include <locale.h>     // biblioteca de alocações de texto por região
-
-int registro()
-{
-	printf("Você escolheu o registro de nomes! \n");
-	system("pause"); 	
-}
-
-int consulta()
-{
-	printf("Você escolheu consultar os nomes! \n");
-	system("pause");	
-}
-
-int deletar()
-{
-	printf("Você escolheu deletar nomes! \n");
-	system("pause");	
-}
-
-
 int main () 
   	{ 
     int opcao=0;        //Definindo variáveis com valor inicial = 0. // 1 igual(=) esta atribuindo o valor
@@ -37,7 +17,7 @@ int main ()
       	printf("\t1 - Registrar nomes:\n");
      	printf("\t2 - Consultar nomes:\n");
       	printf("\t3 - Deletar nomes:\n\n\n");
-	 	printf("Opção:  ");                     // Fim do menu 
+	 	printf("Opção: ");                     // Fim do menu 
       
       	scanf("%d", &opcao); // Scanf -> Armazenando na variavel opcao o valor que o usuario digitar. %d-> Armazenar variavel do tipo inteiro 
                            
@@ -47,15 +27,18 @@ int main ()
       	switch(opcao) // Estamos dizendo: Computador, analisa essa variável pra mim
       	{
       		case 1:  // Dois pontos (:) no Switch case é igual abrir uma chaves {}
-			registro();
+			printf("Você escolheu o registro de nomes!\n");
+    		system("pause"); 
 			break;  // break é como se agente está fechando as chaves. Case 1: (:) abriu e break;  (;) fechou break;
 			
 			case 2: // Caso, a opção for 2
-			consulta();
+			printf("Você escolheu consultar os nomes!\n");
+	   		system("pause");
 			break;
 			
 			case 3:
-			deletar();
+			printf("Você escolheu deletar os nomes!\n"); // Ponto e virgula = ; sempre usar qundo for uma instrução para o computador
+      		system("pause");
 			break;
 			
 			default:
@@ -67,6 +50,3 @@ int main ()
     } 
 	   
 }
-
-   
-   
